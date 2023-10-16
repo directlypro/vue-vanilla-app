60c69e9c85b5985e41b68460b6af49f497c9b6ef
directlypro <moya01107@gmail.com>
2023-10-16 11:53:55 +0200

    my first commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,58 @@
+<script setup></script>
+
+
+<template>
+    <header>
+        <nav class="container">
+            <div class="logo">
+                <RouterLink to="/"><img src="../assets/thedream.jpeg" alt="thedream.logo"></RouterLink>
+                <h1>Into The Dream</h1>
+            </div>
+            <ul class="nav-routes">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            </ul>
+        </nav>
+    </header>
+
+    <body>
+    </body>
+</template>
+
+
+<style lang="scss" scoped>
+header {
+  background-color: #D8D8D8;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
+
+    .logo {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      img {
+        max-width: 50px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,78 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+
+    <div class="homepage">
+      <h1 class="vanilla">Vanilla</h1>
+      <h2>INTO THE DREAM</h2>
+      <div class="album-cover">
+        <img class="logoimg" src="../assets/thedream.jpeg" alt="album-logo">
+      </div>
+      <div class="track-list">
+        <ol>
+          <li>Into The Dream</li>
+          <li>Dreams</li>
+          <li>Lights</li>
+          <li>Never Forget</li>
+          <li>Summer Night</li>
+          <li>Maybe Tomorrow</li>
+          <li>NaNa Pt II</li>
+          <li>Overture</li>
+          <li>Do For Love</li>
+          <li>Lovin' You</li>
+          <li>Longing</li>
+          <li>Between Us</li>
+          <li>Go</li>
+          <li>Tell</li>
+          <li>More Than Love</li>
+          <li>How I Feel</li>
+          <li>Cloud City</li>
+          <li>Setsuna</li>
+        </ol>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Fraunces:opsz,wght@9..144,700&family=Jost:wght@500&family=Lato&display=swap');
+
+.homepage {
+  display: flex;
+  background-color: #EC5671;
+  justify-content: center;
+  padding: 20px;
+  flex-direction: column;
+  align-items: center;
+}
+
+.vanilla {
+  display: inline-block;
+  font-family: 'Dancing Script', cursive;
+  font-size: 4pc;
+  font-weight: bolder;
+}
+
+
+h2,
+.vanilla {
+  padding: 10px;
+}
+
+.logoimg {
+  width: 500px;
+}
+
+.track-list {
+  font-size: 20px;
+}
+
+.intro {
+  display: flex;
+  background: #7C9EDE;
+  align-items: center;
+  justify-content: center;
+}
+</style> 
