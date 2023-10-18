6d8eb397f1b232dc52e713e6c8efb8a9eac3cf00
directlypro <moya01107@gmail.com>
2023-10-18 08:16:21 +0200

    Changed theme of the project

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,15 +1,19 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
 
 
 <template>
     <header>
         <nav class="container">
             <div class="logo">
-                <RouterLink to="/"><img src="../assets/thedream.jpeg" alt="thedream.logo"></RouterLink>
-                <h1>Into The Dream</h1>
+                <RouterLink to="/"><img src="../assets/logo.png" alt="thedream.logo" draggable="false"></RouterLink>
+                <h1 to="/">Vanilla</h1>
             </div>
             <ul class="nav-routes">
             <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/music">Music</RouterLink>
+            <RouterLink to="/videos">Videos</RouterLink>
             <RouterLink to="/about">About</RouterLink>
             </ul>
         </nav>
@@ -21,23 +25,26 @@
 
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 header {
   background-color: #D8D8D8;
   nav {
     display: flex;
     align-items: center;
-    padding: 25px 16px;
+    padding: 12px 12px;
 
     .logo {
       display: flex;
       align-items: center;
       gap: 8px;
       img {
-        max-width: 50px;
+        max-width: 60px;
       }
 
       h1 {
-        font-size: 24px;
+        font-size: 36px;
+        font-family: 'Lobster', sans-serif;
       }
     }
 
@@ -49,8 +56,16 @@ header {
       list-style: none;
 
       a {
+        font-family: 'Roboto', sans-serif;
+        font-size: 18px;
+        padding: 0 10px ;
+        font-weight: 500;
         text-decoration: none;
         color: inherit;
+      }
+
+      a :active {
+        color: blue;
       }
     }
   }
--- src/router/index.js
+++ src/router/index.js
@@ -16,8 +16,19 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/music',
+      name: 'music',
+      component: () => import ('../views/MusicView.vue')
+    },
+    {
+      path: '/videos',
+      name: 'videos',
+      component: () => import ('../views/VideosView.vue')
     }
   ]
 })
 
 export default router
+ 
